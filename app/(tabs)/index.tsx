bd2771b7736fb7990d47b48a4cd15b1642ed4ff7
Claire Kwong <clairebkwong@gmail.com>
2026-03-22 17:07:57 -0400

    a real map    git push --set-upstream origin maplibre-ios

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { useBackgroundLocation } from "@/hooks/use-location";
-import { ActivityIndicator, Text, View } from "react-native";
-import MapView, { Marker } from "react-native-maps";
+import { MapView } from "@maplibre/maplibre-react-native";
+import { ActivityIndicator, Text } from "react-native";
 
 export default function HomeScreen() {
   const { location, status, error } = useBackgroundLocation();
@@ -13,25 +13,10 @@ export default function HomeScreen() {
     : { latitude: 40.7128, longitude: 74.006 };
 
   return (
-    <View>
-      <Text>Hellooo</Text>
-      <MapView
-        style={{
-          width: "100%",
-          height: "100%",
-        }}
-        initialRegion={{
-          latitude: 40.7128,
-          longitude: 74.006,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
-        }}
-      >
-        <Marker
-          coordinate={{ latitude, longitude }}
-          title={`last updated ${location ? location.timestamp : 0}`}
-        />
-      </MapView>
-    </View>
+    <MapView
+      style={{
+        flex: 1,
+      }}
+    />
   );
 }
